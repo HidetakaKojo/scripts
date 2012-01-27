97fa8e1aaf9ccd258cd757c03000ce507296f383
Hidetaka Kojo <hidetaka.kojo@gmail.com>
2012-01-27 14:37:22 +0900

    クエリの取得方法の変更
    selsect * from infomation_schema.processlist と show full processlist で
    日本語の扱いに違いがあったため。
    information_schema.processlist は切れる。

--- list_sql_commands.pl
+++ list_sql_commands.pl
@@ -6,31 +6,34 @@ use warnings;
 use Time::HiRes qw/sleep/;
 use DBI;
 use Getopt::Long;
+use Data::Dumper;
 
 my $port = 3306;
 my $num  = 1000;
 my $results = Getopt::Long::GetOptions(
-        "host=s" => \my $host,
-        "port=i" => \$port,
-        "num=i"  => \$num,
+  "host=s"   => \my $host,
+  "port=i"   => \$port,
+  "num=i"    => \$num,
+  "help"     => \my $help,
 );
 sub help {
   print "[USAGE] perl list_sql_commands.pl --host=[hostname] (--port=[port number] --num=[number of trials])\n";
   exit;
 }
 
+help() if $help;
 help() unless $host;
 
 my $dsn = "DBI:mysql:information_schema;host=$host;port=$port";
 my $dbh = DBI->connect($dsn, "root", "", { RaiseError => 1 });
-my $sth = $dbh->prepare("select INFO from processlist");
+my $sth = $dbh->prepare("show full processlist");
 my %appearance_count = ();
 foreach my $cnt ( 0 .. ($num-1) ) {
   $sth->execute();
-  ROW_LOOP: while (my $row = $sth->fetchrow_arrayref() ) {
-    my $query = $row->[0];
+  ROW_LOOP: while (my $row = $sth->fetchrow_hashref() ) {
+    my $query = $row->{Info};
     next ROW_LOOP if !$query;
-    next ROW_LOOP if $query eq 'select INFO from processlist';
+    next ROW_LOOP if $query eq 'show full processlist';
     $appearance_count{query_normalize($query)}++;
   }
   sleep(0.1);
@@ -45,7 +48,7 @@ foreach my $query ( @appearance_ranking ) {
 sub query_normalize {
   my $query = shift;
   $query = lc $query;
-  $query =~ s/[\s\n]+/ /g;
+  $query =~ s/[\s\r\n]+/ /gs;
   $query =~ s/([\'\"]).+?([\'\"])/$1?$2/g;
   $query =~ s/in\s?\([^\)]*\)/in (?,...,?)/g;
   $query =~ s/([^a-z])\d+$/$1?/g;
